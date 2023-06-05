2252686367ee4ef08e6339d16c2a67a3b781aba8
Abbas <meerabbas110@gmail.com>
2023-06-05 12:02:37 +0530

    all the information added

--- Dom-pratice/dom.js
+++ Dom-pratice/dom.js
@@ -5,4 +5,15 @@
 /**
  * ont the other hand innerHTML will throw back all the comple content of the line what and all attribuets are been used
  so basically it suppor the tags not just the text
+ we do have acess to select the item by query selcto
+ and we can store them into the variable and even more selectoers
+
+ */
+
+ /*
+ while selecting from query selectorAll we get back the nodelist 
+ the nodelist is not a array but lit bit similar because we can use the forEach method there 
+ but when it come for getelemetn byclassname we get back html collection that is not array no method can work on it
+ so we do Array.from(selected element)
+ it will convert it to the array list
  */
