4f6a2c3798b493ee1e2413ba66972eec88d63984
Abbas <meerabbas110@gmail.com>
2023-06-05 11:12:56 +0530

    new dom file

---
+++ Dom-pratice/dom.js
@@ -0,0 +1,8 @@
+//so the diffrence between innerText and textcontent is
+/*innertext will just show the values that are visible on the page
+/**while textcontent will show all the text pressent in the particular tag */
+//so it depend on the use case that if we wana use the display none property also so text content will help
+/**
+ * ont the other hand innerHTML will throw back all the comple content of the line what and all attribuets are been used
+ so basically it suppor the tags not just the text
+ */
